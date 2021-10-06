892b04bfb82bb836e1a11dd91b58e9e3d6dd35e8
Akash <69571641+rushkinrenny2@users.noreply.github.com>
2021-10-06 20:55:17 +0530

    main function completed

--- EqualSumPartition.java
+++ EqualSumPartition.java
@@ -4,7 +4,13 @@ import java.util.Scanner;
 
 public class EqualSumPartition {
     public static void main(String[] args) {
-        
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=scn.nextInt();
+        }
+        System.out.println(equalPartition(n,arr));
 
     }
     public static int equalPartition(int N, int arr[])
